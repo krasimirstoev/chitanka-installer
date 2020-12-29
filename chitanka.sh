97a5e96bbec6fbfe5f39ed2143bec6e30d928917
Krasimir Stoev <alabalism@gmail.com>
2020-12-29 18:37:12 +0200

    changes

--- chitanka.sh
+++ chitanka.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+## General
 installer_git=https://github.com/chitanka/chitanka-installer.git
 installer_dir=${installer_dir:-/root/chitanka-installer}
 install_log=`dirname $0`/install.log
@@ -7,9 +8,9 @@ chitanka_dir=/var/www/chitanka
 chitanka_git='https://github.com/chitanka/chitanka-production.git'
 chitanka_rsync_content='rsync.chitanka.info::content'
 default_domain='chitanka.local'
-debian_stable_version='buster'
+debian_stable_version='10'
 
-## Database section
+## Database
 mysql_service_password='cH-00-service_paS$W'
 mysql_ch_user='chitanka'
 mysql_ch_password='chitanka'
@@ -17,6 +18,12 @@ mysql_ch_database='chitanka'
 mysql_db_dump='http://download.chitanka.info/chitanka.sql.gz'
 mysql_root="mysql -uroot -p${mysql_service_password}"
 mysql_chitanka="mysql -u${mysql_ch_user} -p${mysql_ch_user_password} ${mysql_ch_database}"
+
+## Nginx 
+nginx_chitanka_vhost='chitanka.conf'
+nginx_dir='/etc/nginx/'
+nginx_vhosts_available='${nginx_dir}sites-available/'
+nginx_vhost_enabled='${nginx_dir}sites-enabled/'
 
 install_pkg='apt install -y'
 
@@ -29,29 +36,59 @@ color_reset='\033[0m'
 
 ##################################
 
+pre_install_checks(){
+
+local debian_version=$(sed 's/\..*//' /etc/debian_version)
+
+# is debian based?
+if [ ! -f /etc/debian_version ]; then
+	clear
+	color_echo $color_bold_red "Използваната GNU/Linux дистрибуция не е Debian."
+	log "[Прединсталационна проверка] Използваната дистрибуция не е Debian: липсва /etc/debian_version"
+	exit 1;
+else
+	clear
+	color_echo $color_bold_green "Използваната дистрибуция е Debian базирана."
+	sleep 5
+fi
+
+# is current version is supported?
+if [ $debian_version != $debian_stable_version ]; then
+	clear
+	color_echo $color_bold_red "Версията на използваната Debian дистрибуция не се поддържа от инсталатора."
+	log "[Прединсталационна проверка] Текущата версия на Debian ($debian_version) дистрибуция е различна от поддържаната: $debian_stable_version"
+	exit 1;
+else
+	clear
+	color_echo $color_bold_green "Текущата версия на Debian дистрибуцията се поддържа."
+
+fi
+
+# are you root?
+if [ "$(id -u)" != "0" ]; then
+	clear
+	color_echo $color_bold_red "Инсталаторът трябва да бъде стартиран с потребител ${color_bold_white}root${color_reset}. Следва изход." 1>&2
+	log "[Прединсталационна проверка] Инсталацията е стартирана с потребите: `whoami`"
+	exit 1
+fi
+
+}
+
 install() {
-	# only root is allowed to execute the installer
-	if [ "$(id -u)" != "0" ]; then
-		color_echo $color_bold_red "Инсталаторът трябва да бъде стартиран с потребител ${color_bold_white}root${color_reset}. Следва изход." 1>&2
-		exit 1
-	fi
 
-	if ! is_debian_based; then
-		color_echo $color_bold_red "Използваната версия на Debian не е сред поддържаните. Следва изход."
-		exit 1
-	fi
+	pre_install_checks
 
-	log "Начало на инсталацията"
+	log "[Инсталация] Начало на инсталацията"
 
 	clear
 	splash_screen
 
-	color_echo $color_bold_green "Желаете ли процедурата по инсталация да започне? Изберете y (да) или n (не)."
+	color_echo $color_bold_green "Желаете ли да бъде стартирана процедура по инсталация на системата? Изберете y (да) или n (не)."
 	read yn
 	yn=${yn:-y}
 	if [ "$yn" != "y" ]; then
-		color_echo $color_bold_red "Избрахте да прекратите процедурата по инсталация на огледалото. Следва изход."
-		log "Инсталацията e прекратена по желание на потребителя."
+		color_echo $color_bold_red "Избрахте да прекратите процедурата по инсталация на огледалото."
+		log "[Инсталация] Процесът по инсталация e прекратен по желание на потребителя."
 		exit
 	fi
 
@@ -142,59 +179,110 @@ splash_screen () {
 }
 
 update_system () {
-	color_echo $color_bold_green "Започва обновяване на операционната система."
+	color_echo $color_bold_green "Започва обновяване на пакетната информация."
 	sleep 1
 	apt update -y
-	log "Операционната система беше обновена."
+	log "[Инсталация] Обновяване на пакетната информация."
 }
 
 install_basic_packages () {
 	color_echo $color_bold_green "Инсталация на системен софтуер."
 	sleep 2
-	$INSTALL_PKG git curl rsync
-	log "Инсталиран е необходимият системен софтуер."
-	if [ ! -d $INSTALLER_DIR ]; then
-		git clone $INSTALLER_GIT $INSTALLER_DIR
+	$install_pkg curl rsync
+	log "[Инсталация] Системни пакети: curl; rsync"
+	if [ ! -d $installer_dir ]; then
+		git clone $installer_git $installer_dir
 	fi
 }
 
 install_web_server () {
 	color_echo $color_bold_green "Започва инсталацията на уеб сървъра."
 	sleep 2
-	$INSTALL_PKG nginx php-fpm php-gd php-curl php-xsl php-intl php-zip
-	cp $INSTALLER_DIR/nginx-vhost.conf /etc/nginx/sites-enabled/chitanka
+	$install_okg nginx php-fpm php-gd php-curl php-xsl php-intl php-zip
+	log "[Инсталация] Инсталирани са пакетите свързани с работата на уеб сървъра."
+	#cp $installer_dir/nginx-vhost.conf /etc/nginx/sites-enabled/chitanka
+}
+
+generate_nginx_vhost(){
+
+	color_echo $color_bold_white "Генериране на Nginx съвместим виртуален хост"
+	log "[Инсталация] Генериране на Nginx виртуален хост"
+	local php_locate_sockfile=$(cd /etc/php/; grep -ar "php7.*-fpm.sock" | awk {'print $3'})
+	log "[Инсталация] Пълен път към sock файла на PHP: $php_locate_sockfile"
+
+	color_echo $color_bold_white "По подразбиране, в конфигурацията е заложен домейн ${default_domain}. В случай че разполагате със собствен домейн, бихте могли да го използвате за конфигурацията на огледалото."
+	echo
+	color_echo $color_bold_white "Желаете ли да използвате свой домейн? Изберете (y) за да посочите свой домейн или (n) за да продължи инсталацията с домейна ${default_domain}."
+
+	read yn
+	yn=${yn:-y}
+	if [ "$yn" = "n" ]; then
+		color_echo $color_bold_green "Избрахте да използвате служебното име ${default_domain}. Инсталацията продължава."
+		log "[Инсталация] Избран домейн за инсталацията: служебно (${default_domain})."
+	else
+		color_echo $color_bold_white "Моля, въведете желания домейн:"
+		read own_domain_name
+		color_echo $color_bold_red "Избрахте домейн: $own_domain_name"
+		default_domain="${own_domain_name}"
+		set_domain_in_localhost $default_domain
+		log "[Инсталация] Избран е различен от заложения домейн: $default_domain и ще бъде добавен в конфигурационните файлове."
+	fi
+
+	if [ -d $nginx_vhosts_available ]; then
+		cat > ${nginx_vhosts_available}$nginx_chitanka_vhost <<EOF
+	server {
+	listen 80;
+
+	server_name $default_domain;
+	root /var/www/chitanka/web;
+
+	access_log /var/log/nginx/$default_domain-access.log;
+	error_log /var/log/nginx/$default_domain-error.log;
+
+	location / {
+		index index.php;
+		try_files $uri $uri/ /index.php$is_args$args;
+	}
+
+	location ~ /(index|index_dev)\.php($|/) {
+		# via a unix socket
+		fastcgi_pass unix:$php_locate_sockfile;
+		# via an ip address
+		#fastcgi_pass 127.0.0.1:9000;
+		fastcgi_split_path_info ^(.+\.php)(/.*)$;
+		include fastcgi.conf;
+	}
+
+	location ~ /(css|js|thumb) {
+		expires 30d;
+		try_files /cache$request_uri @asset_generator;
+	}
+	location @asset_generator {
+		rewrite ^/(css|js|thumb)/(.+) /$1/index.php?$2;
+	}
+
+	location ~* \.(eot|otf|ttf|woff)$ {
+		add_header Access-Control-Allow-Origin *;
+	}
+}
+EOF
+
+	restart_web_server
+	log "[Инсталация] Рестартиране на уеб сървъра."
+	else
+		color_echo $color_bold_red "Директорията $nginx_vhosts_available не е налична и инсталацията не може да продължи."
+		log "[Инсталация] Директорията с наличните виртуални хостове ($nginx_vhosts_available) не е налична и инсталацията е прекратена."
+		exit 1;
+	fi
 }
 
 restart_web_server () {
 	service nginx restart
 }
 
-set_domain () {
-	color_echo $color_bold_white "По подразбиране, в конфигурацията е заложен домейн ${DEFAULT_DOMAIN}. В случай че разполагате със собствен домейн, бихте могли да го използвате за конфигурацията на огледалото."
-	echo
-	color_echo $color_bold_white "Желаете ли да използвате свой домейн? Изберете (y) за да посочите свой домейн или (n) за да продължи инсталацията с домейна ${DEFAULT_DOMAIN}."
-
-	read yn
-	yn=${yn:-y}
-	if [ "$yn" = "n" ]; then
-		color_echo $color_bold_green "Избрахте да използвате служебното име ${DEFAULT_DOMAIN}. Инсталацията продължава."
-		log "Избран домейн за инсталацията: служебно (${DEFAULT_DOMAIN})."
-		set_domain_in_localhost $DEFAULT_DOMAIN
-		log "Избран е заложеният по подразбиране домейн ${DEFAULT_DOMAIN}."
-	else
-		color_echo $color_bold_white "Моля, въведете желания домейн:"
-		read own_domain_name
-		color_echo $color_bold_red "Избрахте домейн: $own_domain_name"
-		set_domain_in_webhost $own_domain_name
-		set_domain_in_localhost $own_domain_name
-		log "Избран е различен от заложения домейн: $own_domain_name и е добавен в конфигурационните файлове."
-	fi
-	restart_web_server
-	log "Виртуалният хост беше създаден."
-}
 
 set_domain_in_webhost () {
-	sed -i "s/${DEFAULT_DOMAIN}/$1/g" /etc/nginx/sites-enabled/chitanka
+	sed -i "s/${default_domain}/$1/g" /etc/nginx/sites-enabled/chitanka
 }
 
 set_domain_in_localhost () {
@@ -204,23 +292,23 @@ set_domain_in_localhost () {
 install_db_server () {
 	color_echo $color_bold_green "Инсталация на база от данни MariaDB."
 	sleep 2
-	debconf-set-selections <<< "mariadb-server mysql-server/root_password password $MYSQL_SERVICE_PASSWORD"
-	debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $MYSQL_SERVICE_PASSWORD"
-	$INSTALL_PKG mariadb-server mariadb-client php-mysql
-	log "Инсталирана е база от данни MariaDB със служебна парола: $MYSQL_SERVICE_PASSWORD"
+	debconf-set-selections <<< "mariadb-server mysql-server/root_password password $mysql_service_password"
+	debconf-set-selections <<< "mariadb-server mysql-server/root_password_again password $mysql_service_password"
+	$install_pkg mariadb-server mariadb-client php-mysql
+	log "Инсталирана е база от данни MariaDB със служебна парола: $mysql_service_password"
 }
 
 create_chitanka_db () {
 	color_echo $color_bold_green "Създаване на потребителско име и база от данни за огледалото."
 	sleep 2
-	$MYSQL_ROOT -e "CREATE USER '$MYSQL_CH_USER'@'localhost' IDENTIFIED BY '$MYSQL_CH_USER_PASSWORD'"
-	$MYSQL_ROOT -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_CH_USER'@'localhost'"
-	$MYSQL_ROOT -e "FLUSH PRIVILEGES"
-	$MYSQL_ROOT -e "CREATE DATABASE $MYSQL_CH_DATABASE"
-	log "Създаден е MySQL потребител със служебна парола: $MYSQL_CH_USER_PASSWORD"
-	log "Създадена е MySQL база от данни: $MYSQL_CH_DATABASE"
+	$mysql_root -e "CREATE USER '$mysql_ch_user'@'localhost' IDENTIFIED BY '$mysql_ch_user_password'"
+	$mysql_root -e "GRANT ALL PRIVILEGES ON *.* TO '$mysql_ch_user'@'localhost'"
+	$mysql_root -e "FLUSH PRIVILEGES"
+	$mysql_root -e "CREATE DATABASE $mysql_ch_database"
+	log "Създаден е MySQL потребител със служебна парола: $mysql_ch_user_password"
+	log "Създадена е MySQL база от данни: $mysql_ch_database"
 
-	curl $MYSQL_DB_DUMP | gunzip | $MYSQL_CHITANKA
+	curl $mysql_db_dump | gunzip | $mysql_chitanka
 	log "Базата от данни за огледалото е внесена."
 }
 
@@ -228,13 +316,13 @@ install_chitanka_software () {
 	color_echo $color_bold_green "Вземане на кода от хранилището в GitHub."
 	sleep 2
 
-	rm -rf $CHITANKA_DIR
-	git clone --depth 1 $CHITANKA_GIT $CHITANKA_DIR
+	rm -rf $chitanka_dir
+	git clone --depth 1 $chitanka_git $chitanka_dir
 	log "Програмният код е успешно клониран от хранилището в GitHub."
 
-	cp $INSTALLER_DIR/parameters.yml $CHITANKA_DIR/app/config
+	cp $installer_dir/parameters.yml $chitanka_dir/app/config
 
-	cd $CHITANKA_DIR
+	cd $chitanka_dir
 	chmod -R a+w var/cache var/log var/spool web/cache
 	log "Правата за директориите cache, log и spool са променени."
 }
@@ -257,7 +345,7 @@ rsync_content () {
 	color_echo $color_bold_green "Сваляне на съдържанието."
 	sleep 2
 	log "rsync процедурата е СТАРТИРАНА"
-	rsync -avz --delete ${CHITANKA_RSYNC_CONTENT}/ $CHITANKA_DIR/web/content
+	rsync -avz --delete ${chitanka_rsync_content}/ $chitanka_dir/web/content
 	log "rsync процедурата ПРИКЛЮЧИ"
 }
 
@@ -272,22 +360,8 @@ color_echo () {
 }
 
 log () {
-	logfile=${2:-$INSTALL_LOG}
+	logfile=${2:-$install_log}
 	echo "[`date +"%d.%m.%Y %T"`] $1" >> $logfile
-}
-
-is_debian_stable () {
-	if [[ ! `grep 'VERSION=' /etc/os-release | grep $debian_stable_version` ]]; then return 1; fi
-	
-}
-is_ubuntu () {
-	if [[ ! `grep 'ID=' /etc/os-release | grep ubuntu` ]]; then return 1; fi
-}
-is_debian_based () {
-	if [[ ! -e /etc/debian_version ]]; then return 1; fi
-}
-is_centos () {
-	if [[ ! `grep 'ID=' /etc/os-release | grep centos` ]]; then return 1; fi
 }
 
 is_apache_installed () {
@@ -311,7 +385,7 @@ case "$1" in
 		addcron
 	;;
 	dev)
-		is_ubuntu
+		generate_nginx_vhost
 	;;
 	*)
 		show_help
